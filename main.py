36b17ec0e1c2da2f5a2595310b7301a60a9da8ab
gololobov <gololobov.serhiy@gmail.com>
2026-09-06 14:47:01 +0300

    06.09.2026 / 2

--- main.py
+++ main.py
@@ -1,7 +1,41 @@
-print("Hello Python")
-print("Hello Python")
-print("Hello Python")
-print("Hello Python")
-print("Hello Python")
-print("Hello Python")
-print("Hello Python")
+
+class Student:
+    count = 0
+    def __init__(self, name, age, height=150):
+        self.height = height
+        self.name = name
+        self.age = age
+        #print(f"Hello, i am {self.name}")
+        Student.count += 1
+
+    def __str__(self):
+        return (f"Мене звати {self.name}\n" +
+                f"Мій зріст {self.height} см\n" +
+                f"Вік {self.age} років\n")
+
+    def grow(self, to_height=1):
+        if self.height + to_height < 190:
+            self.height += to_height
+
+    def to_growup(self):
+        if self.age < 55:
+            self.age += 1
+
+
+
+
+print(Student.count)
+
+st = Student("Vasya", 15)
+st.grow(10)
+print(st)
+
+st.to_growup()
+print(st)
+
+print(Student.count)
+
+st1 = Student("Petya", 16, 155)
+print(st)
+
+print(Student.count)
